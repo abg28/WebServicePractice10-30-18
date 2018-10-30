6e5a5d3f8786b95c85e755bbb91660b9bb794aad
Alex Guevara <abg28@duke.edu>
2018-10-30 16:14:43 -0400

    Testing how pytest pep8 likes continuation character

--- funStuff.py
+++ funStuff.py
@@ -1,6 +1,6 @@
 import requests
 
-r = requests.post("http://bme590.suyash.io/student",
+r = requests.post("http://bme590.suyash.io/student", \
                   json={"first_name": "Alex", "last_name": "Guevara", "netid": "abg28", "github_username": "abg28"})
 print(r.json())
 
